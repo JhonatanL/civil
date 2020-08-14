b0565f8f0b09f74676c1df749bf3d6184604a08a
JhonatanL <jhonatandef17@gmail.com>
2020-08-14 11:17:51 -0500

    modificando contraste

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(".btn-menu").click(function(){
 	$(".menu").toggleClass('active');
 });
+
+caches.open(version)
+	.then(function(cache) {
+		return cache.addAll([
+			'/',
+			'index.html',
+			'../css/app.css',
+			'/js/app.min.js',
+			'/assets/images/logo_target_white.png',
+			'/scripts/angular-jwt.min.js',
+			'/scripts/ng-file-upload.min.js',
+		// this guy here
+		'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+	]);
+})
